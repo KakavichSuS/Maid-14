9ffefda5e0bb966ed55d89c8d380e7355cc89bb5
OnsenCapy <101037138+onsencapy@users.noreply.github.com>
2026-01-13 01:50:54 +0600

    Remove Prison map from game entirely (#5717)
    * Revert prison
    
    * Plant
    
    (cherry picked from commit 97961ca145c9daa179193243a507a3e5924b69fe)

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -193,8 +193,7 @@ namespace Content.IntegrationTests.Tests
             "/Maps/_Goobstation/Shuttles/retort_medical.yml",
             "/Maps/_Goobstation/Shuttles/retort_engineering.yml",
             "/Maps/_Goobstation/Shuttles/retort_janitorial.yml",
-            "/Maps/_Goobstation/Shuttles/retort_cburn.yml",
-            "/Maps/_Goobstation/prison.yml"
+            "/Maps/_Goobstation/Shuttles/retort_cburn.yml"
         };
 
         private static readonly string[] GameMaps =
@@ -235,7 +234,6 @@ namespace Content.IntegrationTests.Tests
               "TestTeg",        //Dev map
               "Train",          //Not in pool
               "Packed",
-              "Prison",
               "Reach",
               "Saltern",
               "Serpentcrest",
