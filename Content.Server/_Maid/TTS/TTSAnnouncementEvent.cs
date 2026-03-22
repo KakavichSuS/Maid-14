6735886c2393bad0263900fd8a5fe787a7c2fa83
RedBurningPhoenix <Warete4@gmail.com>
2026-03-23 02:25:47 +0500

    Char cap restriction for Announcement and summ. + params.

--- Content.Server/_Maid/TTS/TTSAnnouncementEvent.cs
+++ Content.Server/_Maid/TTS/TTSAnnouncementEvent.cs
@@ -1,5 +1,13 @@
 namespace Content.Server._Maid.TTS;
 
+/// <summary>
+/// When an announcement is placed on the station, this plays TTS
+/// to the nearest players using the objects specified in <see cref="TTSSystem"/>
+/// </summary>
+/// <param name="message"></param>
+/// <param name="voiceId"></param>
+/// <param name="source"></param>
+/// <param name="global"></param>
 public sealed class TTSAnnouncementEvent(string message, string voiceId, EntityUid source, bool global)
     : EntityEventArgs
 {
