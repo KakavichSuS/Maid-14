562280ffed7628e39d385f1783aa04ca7190fbd0
RedBurningPhoenix <Warete4@gmail.com>
2026-03-23 14:45:21 +0500

    param's

--- Content.Server/_Maid/TTS/TTSAnnouncementEvent.cs
+++ Content.Server/_Maid/TTS/TTSAnnouncementEvent.cs
@@ -4,10 +4,12 @@ namespace Content.Server._Maid.TTS;
 /// When an announcement is placed on the station, this plays TTS
 /// to the nearest players using the objects specified in <see cref="TTSSystem"/>
 /// </summary>
-/// <param name="message"></param>
-/// <param name="voiceId"></param>
-/// <param name="source"></param>
-/// <param name="global"></param>
+/// <param name="message">The announcement text.</param>
+/// <param name="voiceId">Identifier of the TTS voice prototype to use <see cref="TTSVoicePrototype"/>.</param>
+/// <param name="source">The entity that is the source of the announcement.
+/// Used to determine the station when the announcement is not global.</param>
+/// <param name="global">If true, the announcement is broadcast to all players.
+/// If false, it is played only to players on the same station as the source entity.</param>
 public sealed class TTSAnnouncementEvent(string message, string voiceId, EntityUid source, bool global)
     : EntityEventArgs
 {
